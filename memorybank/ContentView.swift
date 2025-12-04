f04b3ea05ffc04a148810e703733730fcdf74e9d
김태우 <taewoo080401@gmail.com>
2025-12-04 16:56:47 +0900

    Initial Commit

---
+++ memorybank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  memorybank
+//
+//  Created by cher1shRXD on 12/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ memorybank/memorybankApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorybankApp.swift
+//  memorybank
+//
+//  Created by cher1shRXD on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct memorybankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
